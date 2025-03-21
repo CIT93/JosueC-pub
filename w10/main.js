0ad9850d2eb203364cc4e8f3206fb8efeb1bc418
Josue Cisneros <0961018@my.scccd.edu>
2025-03-21 03:36:26 -0700

    w10 my attempt at the code, before I review other students code

--- w10/main.js
+++ w10/main.js
@@ -3,7 +3,7 @@ import {determineHouseHoldPts, determineHouseSizePts } from './cfp.js';
 import {FORM, FNAME, LNAME, SUBMIT} from "./global.js"
 import {saveLS, cfpData} from "./storage.js"
 
-const start = function(first, last, houseHoldMembers, houseSize) {
+const start = (first, last, houseHoldMembers, houseSize) => {
   const houseHoldPTS = determineHouseHoldPts(houseHoldMembers);
   const houseSizePTS = determineHouseSizePts(houseSize);
   const total = houseHoldPTS + houseSizePTS;
@@ -21,7 +21,7 @@ const start = function(first, last, houseHoldMembers, houseSize) {
 renderTbl(cfpData);
 
 // Function to validate a single field
-const validateField= function (event) {
+const validateField = event  => {
   const field = event.target.value;
   const fieldId = event.target.id;
   const fieldError = document.getElementById(`${fieldId}Error`)
@@ -39,7 +39,7 @@ const validateField= function (event) {
 FNAME.addEventListener(`blur`, validateField);
 LNAME.addEventListener(`blur`, validateField);
 
-FORM.addEventListener(`submit`, function(e){
+FORM.addEventListener(`submit`, e => {
   e.preventDefault();
   if (FNAME.value !== `` && LNAME.value !== ``) {
     SUBMIT.textContent = ``;
@@ -52,13 +52,18 @@ FORM.addEventListener(`submit`, function(e){
   }
 });
 
-const add2 = function(...a) {
-  return 2 + a[3];
-}
+//rest operator 
+// const add2 = function(...a) {
+//   return 2 + a[3];
+// }
 
-const result = add2(1, 2, 3, 4);
+// const result = add2(1, 2, 3, 4);
 
-//spread argrument
+// arrow function
+const add2 = a => 2 + a;
+
+
+const result = add2(100);
 
 //IIFE
 
